81c504718657f5433d5190a3baaee60fa87c1337
nithyanands <nithyanand.s@outlook.com>
2026-07-11 21:02:20 +0100

    Add POST /tickets endpoint to create tickets and store them inline -self

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from datetime import datetime
 app = Flask(__name__)
+# In-memory storage for tickets
+tickets = []
+next_id = 1
+
+# Test route to check if the API is running and accessible on web and postman
 
 @app.route('/')
 def home():
     return {"message": "Cyberock Issue Tracker API is test running"}
+
+# Create a new ticket record from Postman request body
+
+@app.route('/tickets' , methods=['POST'])
+def create_ticket():
+    global next_id
+    ticket_data = request.get_json()
+    new_ticket = {
+        "ticket_id": next_id,
+        "title": ticket_data.get("title"),
+        "description": ticket_data.get("description"),
+        "severity": ticket_data.get("severity"),
+        "status": ticket_data.get("status", "open"),
+        "date_reported": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "reported_by": ticket_data.get("reported_by"),
+        "assigned_to": ticket_data.get("assigned_to"),
+        "closed_date": None
+    }
+    tickets.append(new_ticket)
+    next_id += 1
+    return jsonify(new_ticket), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
