ffad3bdc93e0371771d5e2b079b13c663a5d129c
nithyanands <nithyanand.s@outlook.com>
2026-07-12 16:19:05 +0100

    Add PUT /tickets/id endpoint to update ticket informations -self

--- app.py
+++ app.py
@@ -33,15 +33,32 @@ def create_ticket():
     return jsonify(new_ticket), 201
 
 # get all tickets
+
 @app.route('/tickets', methods=['GET'])
 def get_all_tickets():
     return jsonify(tickets), 200
 
 # get a specific ticket by ID
+
 @app.route('/tickets/<int:ticket_id>',methods=['GET'])
 def get_specific_ticket(ticket_id):
     for ticket in tickets:
         if ticket["ticket_id"] == ticket_id:
+            return jsonify(ticket), 200
+    return jsonify({"error": "Ticket not found"}), 404
+
+# update a specific ticket by ID
+
+@app.route('/tickets/<int:ticket_id>', methods=['PUT'])
+def update_ticket(ticket_id):
+    ticket_data = request.get_json()
+    for ticket in tickets:
+        if ticket["ticket_id"] == ticket_id:
+            ticket["title"] = ticket_data.get("title", ticket["title"])
+            ticket["description"] = ticket_data.get("description", ticket["description"])
+            ticket["severity"] = ticket_data.get("severity", ticket["severity"])
+            ticket["status"] = ticket_data.get("status", ticket["status"])
+            ticket["assigned_to"] = ticket_data.get("assigned_to", ticket["assigned_to"])
             return jsonify(ticket), 200
     return jsonify({"error": "Ticket not found"}), 404
 
